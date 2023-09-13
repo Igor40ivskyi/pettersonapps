7080eeed3199579c313074fded985eba71786416
Admin <sorokivskyi777@gmail.com>
2023-09-13 19:12:16 +0300

    initial commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Post, Req } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
-  @Get()
-  async getUserList() {
-    return 'List';
-  }
-
-  @Post()
-  async createUserAccount() {
-    return 'New User';
+  @Post('/')
+  async createUserAccount(@Body() body: any) {
+    return 'xxx';
   }
 }
