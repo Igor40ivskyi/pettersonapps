f5fd21ec53c1548bebebf9abdfc0f76619d2fb9d
Admin <sorokivskyi777@gmail.com>
2023-09-13 20:19:25 +0300

    have done the env

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.POSTGRES_DATABASE);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
