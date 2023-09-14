6697219e17d921eed95feb828ffff6dbd576a2f6
Admin <sorokivskyi777@gmail.com>
2023-09-14 12:30:22 +0300

    have done the user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  firstName: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  lastName: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  nickName: string;
+
+  @Column({ type: 'int', nullable: true })
+  age: number;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
